f70efe70a8ea6f549d472aa273a616ba78b78bcd
Israt Jahan <i.jahan@ihsph.org>
2018-01-03 22:55:21 +0000

    finished with my requiredment

--- js/script.js
+++ js/script.js
@@ -6,8 +6,14 @@
 
 // Document Ready Function. All of your jQuery should go in here. 
 $( document ).ready(function() {
-  
-
+$("button").click(function(){
+    var message=$("input").val();
+    var resultshow = ay(message);
+    $("#message").text(resultshow);
+});
+function ay(word){
+ return word + "ay";    
+}
 
 
 });
